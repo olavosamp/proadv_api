44149b89c74e392167862fd821111e3c203872fd
Olavo <olavosamp@poli.ufrj.br>
2021-04-15 18:42:24 -0300

    feat: Add Publication, SearchTerm model and admin
    config: moved data folder inside proadv for container access
    
    config: registered api app in settings

--- proadv/api/admin.py
+++ proadv/api/admin.py
@@ -1,3 +1,11 @@
+from api.models import SearchTerm, Publication
+
 from django.contrib import admin
 
 # Register your models here.
+class SearchTermAdmin(admin.ModelAdmin):
+    list_display = ('classification', 'term')
+    list_filter = ['classification']
+
+admin.site.register(Publication)
+admin.site.register(SearchTerm, SearchTermAdmin)
--- proadv/api/models.py
+++ proadv/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.postgres.indexes import GinIndex
 
 # Create your models here.
+class SearchTerm(models.Model):
+    class Meta:
+        verbose_name = "termo de pesquisa"
+        verbose_name_plural = "termos de pesquisa"
+    term = models.fields.CharField(max_length=100)
+    classification = models.fields.CharField(max_length=60)
+
+class Publication(models.Model):
+    class Meta:
+        verbose_name = "publicação"
+        verbose_name_plural = "publicações"
+
+        # indexes = [GinIndex(fields=['data'])]
+
+    data = models.JSONField(null=True)
