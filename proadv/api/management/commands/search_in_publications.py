d5a61c3e5f9e85bfe20d85caf48964a74959f6e7
Olavo <olavosamp@poli.ufrj.br>
2021-04-16 22:16:13 -0300

    feat: search_in_publications command

---
+++ proadv/api/management/commands/search_in_publications.py
@@ -0,0 +1,25 @@
+from api.models import Publication, SearchTerm
+
+from django.core.management import BaseCommand
+from django.db.models import F
+
+
+class Command(BaseCommand):
+    help = "Search publications for search terms. If found, each term is added to the publication."
+
+    def handle(self, *args, **options):
+        if SearchTerm.objects.count() < 1:
+            raise ValueError("No search terms found.")
+
+        if Publication.objects.count() < 1:
+            raise ValueError("No publications found.")
+
+        for search_term in SearchTerm.objects.all():
+            print("\nTerm: ", search_term.term)
+
+            publications_qs = Publication.objects.filter(data__Conteudo__regex=search_term.term)
+            print(f"Found {len(publications_qs)} matches")
+
+            if publications_qs.count() > 0:
+                print("Adding publications to term")
+                search_term.publications.add(*publications_qs)
